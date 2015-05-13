63e1791a8a71d1d260e8835ac67955aa685b760e
Dan Hoffman <dan@criticalhitlabs.com>
2015-05-13 16:01:21 -0700

    Copy files from ucsf gadgets repo

---
+++ js/environment.js
@@ -0,0 +1,16 @@
+// UNCOMMENT OUT THE BLOCK THAT IS APPROPRIATE FOR YOUR ENVIRONMENT!!!!
+
+//For production
+/************
+var ENV_PROFILES_URL = 'http://profiles.ucsf.edu';
+var ENV_LOCAL_URL = 'http://profiles.ucsf.edu';
+**************/
+
+//For staging
+var ENV_PROFILES_URL = 'http://stage-profiles.ucsf.edu/profiles200';
+var ENV_LOCAL_URL = 'http://stage-profiles.ucsf.edu';
+
+//Institution variables
+var ENV_INST_ABRV = 'UCSF';
+var ENV_INST_EMAIL = "<a href='mailto:profiles@ucsf.edu'>profiles@ucsf.edu</a>";
+var ENV_INST_PUBSCONTACT = "<a href='mailto:profiles@ucsf.edu?subject=Awarded Grant Information in UCSF Profiles'>contact us</a>";
