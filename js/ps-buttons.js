e62bcc9edc67762d039f5b0a42aeaac5e9ef3ff1
Dan Hoffman <dan@criticalhitlabs.com>
2015-06-17 10:25:18 -0700

    Refactor button setup, part 2

--- js/ps-buttons.js
+++ js/ps-buttons.js
@@ -3,7 +3,7 @@ personalStats.buttons = personalStats.buttons || {};
 
 personalStats.buttons.setupToggleButtons = function() {
     console.log("setup toggle buttons");
-    // Set up the toggle buttons
+
     $(".toggle-btn input[type=radio]").addClass("visuallyhidden"); // hide the radio button circle on all buttons
 
     $(".date-toggle .toggle-btn input[type=radio]").change(function() {
@@ -36,4 +36,59 @@ personalStats.buttons.setupToggleButtons = function() {
     // Turn on the first toggle button for each panel
     $(".geo.date-toggle .toggle-btn input[type=radio]").first().parent().addClass("success");
     $(".domain.date-toggle .toggle-btn input[type=radio]").first().parent().addClass("success");
+}
+
+personalStats.buttons.setupMapButtons = function() {
+    console.log("setup map buttons");
+
+    $("#map-world").click(function() {
+        gadgetEventTrack("map_world");
+        if ($(".geo.date-toggle .toggle-btn input[type=radio]").first().parent().hasClass("success")) {
+            drawGeoChart_World(aggregatedByCountryLastMonth);
+        } else {
+            drawGeoChart_World(aggregatedByCountryLastYear);
+        }
+        showOverlay("map-world");
+    });
+
+    $("#map-usa").click(function() {
+        gadgetEventTrack("map_usa");
+        if ($(".geo.date-toggle .toggle-btn input[type=radio]").first().parent().hasClass("success")) {
+            drawGeoChart_USA(aggregatedByStateLastMonth);
+        } else {
+            drawGeoChart_USA(aggregatedByStateLastYear);
+        }
+        showOverlay("map-usa");
+    });
+}
+
+personalStats.buttons.setupSeeAllButtons = function() {
+    // See All (geo) button shows all for Month or Year (based on current selection)
+    $(".geo.show-all.toggle-btn").click(function() {
+        gadgetEventTrack("country_see_all");
+        if ($(".geo.date-toggle .toggle-btn input[type=radio]").first().parent().hasClass("success")) {
+            showViewsByCity(aggregatedByCityLastMonth, "geo-list-all");
+        } else {
+            showViewsByCity(aggregatedByCityLastYear, "geo-list-all");
+        }
+        showOverlay("geo-list");
+    });
+
+    // See All (domain) button shows all for Month or Year (based on current selection)
+    $(".domain.show-all.toggle-btn").click(function() {
+        gadgetEventTrack("domain_see_all");
+        if ($(".domanin.date-toggle .toggle-btn input[type=radio]").first().parent().hasClass("success")) {
+            showViewsByDomain(aggregatedByDomainLastMonth, "domain-list-all");
+        } else {
+            showViewsByDomain(aggregatedByDomainLastYear, "domain-list-all");
+        }
+        showOverlay("domain-list");
+    });
+
+    // See All (related people) button shows all
+    $(".related-people.show-all.toggle-btn").click(function() {
+        gadgetEventTrack("related_people_see_all");
+        showRelatedPeople(aggregatedByAlsoViewed, "related-people-list-all");
+        showOverlay("related-people-list");
+    });
 }
