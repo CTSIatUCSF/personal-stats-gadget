d9fba626426830aa04ccc73905c02d1c4bd6c180
Dan Hoffman <dan@criticalhitlabs.com>
2015-06-17 10:05:53 -0700

    Refactor button setup

--- js/ps-buttons.js
+++ js/ps-buttons.js
@@ -1,8 +1,9 @@
-window.personalStats = window.personalStats || { };
+window.personalStats = window.personalStats || {};
+personalStats.buttons = personalStats.buttons || {};
 
-window.personalStats.buttons.setupToggleButtons = function() {
-    $(".toggle-btn input[type=radio]").addClass("visuallyhidden");                          // hide the radio button circle on all buttons
-    $(".see-more-less .toggle-btn input[type=radio]").parent().addClass("visuallyhidden");  // hide all buttons in see-more-less button groups
+personalStats.buttons.setupToggleButtons = function() {
+    // Set up the toggle buttons
+    $(".toggle-btn input[type=radio]").addClass("visuallyhidden"); // hide the radio button circle on all buttons
 
     $(".date-toggle .toggle-btn input[type=radio]").change(function() {
         if( $(this).attr("name")) {
@@ -12,88 +13,26 @@ window.personalStats.buttons.setupToggleButtons = function() {
         }
     });
 
-    $(".see-more-less .toggle-btn input[type=radio]").change(function() {
-        if( $(this).attr("name")) {
-            $(this).parent().addClass("visuallyhidden").siblings().removeClass("visuallyhidden")
-        } else {
-            $(this).parent().toggleClass("visuallyhidden");
-        }
-    });
-
     $(".geo.date-toggle .toggle-btn input[type=radio]").first().click(function() {
-        console.log("country-month selected");
-        showViewsByCountry(aggregatedByCountryLastMonth, onlyShow);
-        drawGeoChart(aggregatedByCountryLastMonth);
+        gadgetEventTrack("country_toggle");
+        showViewsByCity(aggregatedByCityLastMonth, "geo-list", onlyShow);
     });
 
     $(".geo.date-toggle .toggle-btn input[type=radio]").last().click(function() {
-        console.log("country-year selected");
-        showViewsByCountry(aggregatedByCountryLastYear, onlyShow);
-        drawGeoChart(aggregatedByCountryLastYear);
+        gadgetEventTrack("country_toggle");
+        showViewsByCity(aggregatedByCityLastYear, "geo-list", onlyShow);
     });
     $(".domain.date-toggle .toggle-btn input[type=radio]").first().click(function() {
-        console.log("domain-month selected");
-        showViewsByDomain(aggregatedByDomainLastMonth, onlyShow);
+        gadgetEventTrack("domain_toggle");
+        showViewsByDomain(aggregatedByDomainLastMonth, "domain-list", onlyShow);
     });
 
     $(".domain.date-toggle .toggle-btn input[type=radio]").last().click(function() {
-        console.log("domain-year selected");
-        showViewsByDomain(aggregatedByDomainLastYear, onlyShow);
+        gadgetEventTrack("domain_toggle");
+        showViewsByDomain(aggregatedByDomainLastYear, "domain-list", onlyShow);
     });
 
     // Turn on the first toggle button for each panel
     $(".geo.date-toggle .toggle-btn input[type=radio]").first().parent().addClass("success");
     $(".domain.date-toggle .toggle-btn input[type=radio]").first().parent().addClass("success");
-
-    // See All/Fewer (geo) button shows all for Month or Year (based on current selection)
-    $(".geo.see-more-less .toggle-btn input[type=radio]").click(function() {
-
-        if ($(this).parent().is(':first-child')) { // See All Button 
-            console.log("see all button clicked");
-            
-            if ($(".geo.date-toggle .toggle-btn input[type=radio]").first().parent().hasClass("success")) {
-                console.log("see all button clicked - last month");
-                showViewsByCountry(aggregatedByCountryLastMonth);
-            } else {
-                console.log("see all button clicked - last year");
-                showViewsByCountry(aggregatedByCountryLastYear);
-            }
-        } else { // See Fewer Button
-            console.log("see fewer button clicked");
-            
-            if ($(".geo.date-toggle .toggle-btn input[type=radio]").first().parent().hasClass("success")) {
-                console.log("see fewer button clicked - last month");
-                showViewsByCountry(aggregatedByCountryLastMonth, onlyShow);
-            } else {
-                console.log("see fewer button clicked - last year");
-                showViewsByCountry(aggregatedByCountryLastYear, onlyShow);
-            }
-        }
-    });
-
-    // See All/Fewer (domain) button shows all for Month or Year (based on current selection)
-    $(".domain.see-more-less .toggle-btn input[type=radio]").click(function() {
-
-        if ($(this).parent().is(':first-child')) { // See All Button 
-            console.log("see all button clicked");
-            
-            if ($(".domain.date-toggle .toggle-btn input[type=radio]").first().parent().hasClass("success")) {
-                console.log("see all button clicked - last month");
-                showViewsByDomain(aggregatedByDomainLastMonth);
-            } else {
-                console.log("see all button clicked - last year");
-                showViewsByDomain(aggregatedByDomainLastYear);
-            }
-        } else { // See Fewer Button
-            console.log("see fewer button clicked");
-            
-            if ($(".domain.date-toggle .toggle-btn input[type=radio]").first().parent().hasClass("success")) {
-                console.log("see fewer button clicked - last month");
-                showViewsByDomain(aggregatedByDomainLastMonth, onlyShow);
-            } else {
-                console.log("see fewer button clicked - last year");
-                showViewsByDomain(aggregatedByDomainLastYear, onlyShow);
-            }
-        }
-    });
 }
